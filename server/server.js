00dbc1141da510e62d41ff45ff8a6b1b04eb4526
Najto <ole1510@live.de>
2025-07-14 19:38:45 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3001;
 
 // Middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 app.use(express.static('public'));
 
 // Ensure files directory exists
